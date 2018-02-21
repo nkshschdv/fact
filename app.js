02b91730b6f8c40eb0df1422f9459c4f94ec7970
Deepak Kumar <ankusachdeva@yahoo.com>
2018-02-21 11:51:08 +0530

    Guard added

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 function CalculateFact() {
     var n =
         $('input').val();
-        console.log(n);
-    var result = fact(n);
+    console.log(n);
+    if (n < 0) {
+        document.getElementById("result").innerHTML = " Number should be positive and greater than zero";
+        return;
+    }
+    else if (n > 21) {
+        document.getElementById("result").innerHTML = " Number should be less than 22";
+        return;
+    }
+    else if ( ! ((typeof n )!= "number")){
+        document.getElementById("result").innerHTML = " Type must be whole number";
+        return;
+    }
+
+    if (n > 0 ){
+        var result = fact(n);
     console.log(result);
     document.getElementById("result").innerHTML = result;
-alert(result);
+    }
 }
 function fact(n) {
 
